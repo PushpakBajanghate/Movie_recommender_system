7e9269d5c1be37f163afe3ae4fbd7ba04f3921c4
PushpakBajanghate <166428381+PushpakBajanghate@users.noreply.github.com>
2026-02-28 13:25:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from recommender import recommend, movies
+
+st.title("🎬 Movie Recommender System")
+
+selected_movie = st.selectbox(
+    "Select a movie",
+    movies['title'].values
+)
+
+if st.button("Recommend"):
+    recommendations = recommend(selected_movie)
+
+    st.subheader("Recommended Movies:")
+    for movie in recommendations:
+        st.write(movie)
+        
---
+++ recommender.py
@@ -0,0 +1,23 @@
+import pickle
+
+# load data
+movies = pickle.load(open('model/movies.pkl', 'rb'))
+similarity = pickle.load(open('model/similarity.pkl', 'rb'))
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movies_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    recommended_movies = []
+
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+    return recommended_movies
